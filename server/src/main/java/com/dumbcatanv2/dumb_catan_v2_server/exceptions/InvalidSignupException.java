62b7929b462a83fd8ed1a9a2f2e56c9a42133cad
ryan <ryan.d.matuszak@gmail.com>
2025-07-22 20:49:44 -0500

    removed no longer used exception classes

--- server/src/main/java/com/dumbcatanv2/dumb_catan_v2_server/exceptions/InvalidSignupException.java
+++
@@ -1,7 +0,0 @@
-package com.dumbcatanv2.dumb_catan_v2_server.exceptions;
-
-public class InvalidSignupException extends RuntimeException{
-    public InvalidSignupException(String message) {
-        super(message);
-    }
-}
--- server/src/main/java/com/dumbcatanv2/dumb_catan_v2_server/exceptions/InvalidUsernameException.java
+++
@@ -1,7 +0,0 @@
-package com.dumbcatanv2.dumb_catan_v2_server.exceptions;
-
-public class InvalidUsernameException extends RuntimeException{
-    public InvalidUsernameException(String message) {
-        super(message);
-    }
-}
